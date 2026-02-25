22e70374bceb096c2291b9ecb0214a0cb28eaf10
lamld01 <lamld2510@gmail.com>
2026-02-25 23:04:31 +0700

    refactor(table): add configurable server-side filter component

--- src/app/shared/ui/table/component/table-cell/table-cell.ts
+++ src/app/shared/ui/table/component/table-cell/table-cell.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { TableColumn } from '../../models/table-config.model';
+import { TableAction, TableColumn } from '../../models/table-config.model';
 import { getValueByPath } from '../../utils/object.util';
 
 @Component({
@@ -15,4 +15,17 @@ export class TableCellComponent {
   get value(): any {
     return getValueByPath(this.rowData, this.column.field);
   }
+
+  get actions(): TableAction[] {
+    return this.column.actions ?? [];
+  }
+
+  isActionDisabled(action: TableAction): boolean {
+    return action.disabled?.(this.rowData) ?? false;
+  }
+
+  onActionClick(action: TableAction): void {
+    action.onClick(this.rowData);
+  }
 }
+
